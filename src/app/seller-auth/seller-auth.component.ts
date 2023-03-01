0d6e296a3669b02a94dbcf7d5cd3c13dd3d55de0
Khizer Ali <khizer358@gmail.com>
2023-03-02 00:36:16 +0500

    added auth

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -8,6 +8,8 @@ import { SignUp } from '../data-type';
   styleUrls: ['./seller-auth.component.css']
 })
 export class SellerAuthComponent implements OnInit {
+  showLogin: any = false;
+  authError: string = ''
 constructor(private seller: SellerService, private router:Router){}
 
 
@@ -18,5 +20,22 @@ ngOnInit() {
 
   signUp(data: SignUp){
     this.seller.userSignUp(data)
+  }
+  login(data: SignUp){
+    this.authError = ''
+    this.seller.userLogin(data)
+    this.seller.isLoginError.subscribe((isError)=> {
+      if(isError){
+        this.authError = 'Email or Password is not correct'
+      }
+    })
+  }
+
+
+  openLogin(){
+    this.showLogin = true
+  }
+  openSignUp(){
+    this.showLogin = false
   }
 }
--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,28 +1,53 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
-import { SignUp } from '../data-type';
+import { login, SignUp } from '../data-type';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SellerService {
-  isSellerLoggedIn = new BehaviorSubject<boolean>(false)
+  isSellerLoggedIn = new BehaviorSubject<boolean>(false);
+  isLoginError = new EventEmitter<boolean>(false)
 
-  constructor(private http:HttpClient, private router:Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
-  userSignUp(data: SignUp){
-   this.http.post('http://localhost:3000/seller', data, {observe: 'response'}).subscribe((result)=>{
-    this.isSellerLoggedIn.next(true);
-    localStorage.setItem('seller', JSON.stringify(result.body))
-    this.router.navigate(['/seller-home'])
-    })
+  userSignUp(data: SignUp) {
+    this.http
+      .post('http://localhost:3000/seller', data, { observe: 'response' })
+      .subscribe((result) => {
+        this.isSellerLoggedIn.next(true);
+        localStorage.setItem('seller', JSON.stringify(result.body));
+        this.router.navigate(['/seller-home']);
+      });
   }
 
-  reloadSeller(){
-    if(localStorage.getItem('seller')){
+  reloadSeller() {
+    if (localStorage.getItem('seller')) {
       this.isSellerLoggedIn.next(true);
-      this.router.navigate(['/seller-home'])
-  }}
+      this.router.navigate(['/seller-home']);
+    }
+  }
+
+  userLogin(data: login) {
+    console.log(data);
+    this.http
+      .get(
+        `http://localhost:3000/seller?email=${data.email}&password=${data.password}`,
+        { observe: 'response' }
+      )
+      .subscribe((result: any) => {
+        console.log(result);
+        if(result && result.body && result.body.length) {
+          localStorage.setItem('seller', JSON.stringify(result.body));
+        this.router.navigate(['/seller-home']);
+          console.log('login')
+        }else{
+          this.isLoginError.emit(true)
+        }
+        // this.isSellerLoggedIn.next(true);
+        
+      });
+  }
 }
